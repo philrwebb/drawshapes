232696f006f8d7f9d208ca03fffe7b3bc0ee6b3d
philrwebb <prwebb@optusnet.com.au>
2020-12-23 15:26:33 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,42 @@
+const canvas = document.getElementById('canvas1');
+const ctx = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+ctx.globalCompositeOperation = 'destination-over';
+let hue = Math.random() * 360;
+
+let number = 0;
+let scale = 10;
+let adj = Math.random() * 1.1;
+
+
+const drawFlower = () => {
+    let angle = number * adj;
+    let radius = scale * Math.sqrt(number);
+    let positionX = radius * Math.sin(angle) + canvas.width / 2;
+    let positionY = radius * Math.cos(angle) + canvas.height / 2;
+
+    ctx.fillStyle = 'hsl('+ hue + ', 100%, 50%)';
+    ctx.strokeStyle = 'black';
+    ctx.lineWidth = 4;
+    ctx.beginPath();
+    ctx.arc(positionX, positionY, 10, 0, Math.PI * 2);
+    ctx.closePath();
+    ctx.fill();
+    ctx.stroke();
+    number += 1;
+    hue += 0.5;
+}
+
+const animate = () => {
+    // draw each frame
+    drawFlower();
+    if (number > 1000) 
+    {
+        number = 0;
+        adj = Math.random() * 1.25;
+        ctx.clearRect(0,0, canvas.width, canvas.height);
+    }
+    requestAnimationFrame(animate);
+}
+animate();
